5c8be5d16d83e45ab3d1d551d1ce6ae8e51450dc
Juan Manuel Alemán <juale86@gmail.com>
2018-12-02 14:26:43 -0300

    Task 9 completed.

--- program.js
+++ program.js
@@ -21,17 +21,47 @@
 //process.stdin.pipe(stream).pipe(process.stdout);
 
 // Ej 5
-var split = require('split');
-var through2 = require('through2');
-var lineN = 0;
-process.stdin
-    .pipe(split())
-    .pipe(through2(function (line, _, next) {
-    	if(lineN %2 == 0)
-        	console.log(line.toString().toLowerCase());
-        else 
-        	console.log(line.toString().toUpperCase());
-        lineN++;
-        next();
-    }))
-;
+//var split = require('split');
+//var through2 = require('through2');
+//var lineN = 0;
+//process.stdin
+//    .pipe(split())
+//    .pipe(through2(function (line, _, next) {
+//    	if(lineN %2 == 0)
+//        	console.log(line.toString().toLowerCase());
+//        else 
+//        	console.log(line.toString().toUpperCase());
+//        lineN++;
+//        next();
+//    }))
+//;
+
+// Ej 7
+//const http = require('http')
+//const th = require('through2')
+//
+//function write(buffer, _, next){
+//	this.push(buffer.toString().toUpperCase());
+//	next();
+//}
+//const server = http.createServer((req,res) => {
+//	if(req.method == 'POST'){
+//		req.pipe(th(write))
+//			.pipe(res)
+//	}
+//	else
+//		res.end('Método no permitido')
+//});
+//server.listen(process.argv[2])
+
+// Ej 8:
+//const request = require('request')
+//const r = request.post('http://localhost:8099')
+//process.stdin.pipe(r).pipe(process.stdout)
+
+// Ej 9:
+//var ws = require('websocket-stream');
+//var stream = ws('ws://localhost:8099');
+//stream.write('hello\n');
+
+// Ej 10:
